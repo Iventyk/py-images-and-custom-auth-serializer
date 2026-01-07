8ea87158509122ded39bc87e11d6fcbeba0ae04b
ivan <ivanventik@gmail.com>
2026-01-07 09:59:45 +0200

    edited due to comments

--- user/views.py
+++ user/views.py
@@ -3,8 +3,6 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 
 from user.serializers import UserSerializer, AuthTokenEmailSerializer
 
